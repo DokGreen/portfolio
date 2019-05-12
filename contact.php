31840015b0718c0050f8c5002ae0096ce7708ea8
Reece <rfarnworth92@gmail.com>
2019-05-12 17:23:53 +0100

    missing contact form

--- contact.php
+++ contact.php
@@ -1,8 +1,16 @@
 <?php
-if (isset($_POST['btn00'])) {
+  //vars for form inputs
   $name = $_POST['name'];
   $email = $_POST['email'];
   $subject = $_POST['subject'];
-}
 
+  //send to me
+  $mailTo = "farnworthwork@gmail.com";
+  $mailSubject = "Job opportunity";
+  $headers = "From: " .$email;
+  //$txt gives a nice format to the email \n is an underscore.
+  $txt = "You have recived an email from ".$name."\n".$email".\n\n".$subject;
+  //create the email
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: index.php?mailsend");
 ?>
