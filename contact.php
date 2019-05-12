12df084b0690dae92da65f3afcbcacdaa872d502
Reece <rfarnworth92@gmail.com>
2019-05-12 16:02:48 +0100

    testing the contact form

--- contact.php
+++ contact.php
@@ -1,8 +1,20 @@
 <?php
-if (isset($_POST['btn00'])) {
+//if btn pressed
+if (isset($_POST['btn'])) {
+  //create vars for the form fields
   $name = $_POST['name'];
   $email = $_POST['email'];
   $subject = $_POST['subject'];
+
+//send to me
+  $mailTo = "farnworthwork@gmail.com";
+  $header = "From: " .$email;
+  //$txt gives a nice format to the email \n is an underscore.
+  $txt = "You have recived an email from ".$name.".\n\n".$subject;
+
+  //create the email
+  mail($mailTo, $subject, $txt, $header);
+  header("Location: index.php?mailsend");
 }
 
 ?>
