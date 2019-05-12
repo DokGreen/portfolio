4eec502f4c5fda395c25260682fd2e5d3cfffd10
Reece <rfarnworth92@gmail.com>
2019-05-12 17:15:22 +0100

    more php amendments

--- contact.php
+++ contact.php
@@ -1,6 +1,4 @@
 <?php
-//if btn pressed
-if (isset($_POST['btn'])) {
   //create vars for the form fields
   $name = $_POST['name'];
   $email = $_POST['email'];
@@ -8,13 +6,12 @@ if (isset($_POST['btn'])) {
 
 //send to me
   $mailTo = "farnworthwork@gmail.com";
-  $header = "From: " .$email;
+  $mailSubject = "Job opportunity";
+  $headers = "From: " .$email;
   //$txt gives a nice format to the email \n is an underscore.
-  $txt = "You have recived an email from ".$name.".\n\n".$subject;
+  $txt = "You have recived an email from ".$name."\n".$email".\n\n".$subject;
 
   //create the email
-  mail($mailTo, $subject, $txt, $header);
+  mail($mailTo, $subject, $txt, $headers);
   header("Location: index.php?mailsend");
-}
-
 ?>
