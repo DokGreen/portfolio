62e99db15144e27f0cd91063170d18df629c1e2f
Reece <rfarnworth92@gmail.com>
2019-06-01 18:01:27 +0100

    minor style changes + js bars complete

--- resources/port.js
+++ resources/port.js
@@ -13,3 +13,31 @@ function mousemove(){
 document.addEventListener('scroll', function(){ // on page scroll
   requestAnimationFrame(mousemove) // call mousemove  the animatione frame helps to smooth it out
 })
+
+
+/*------------------------------------TEST*/
+const bars = document.querySelector("#bar");
+const bar1 = document.querySelectorAll('.bartxt');
+const isInViewport = function (elem) {
+	const distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+	);
+};
+
+window.addEventListener('scroll', function (event) {
+  if (isInViewport(bars)) {
+		bar1[0].style.width = "80%"
+    bar1[1].style.width = "80%";
+    bar1[2].style.width = "60%";
+    bar1[3].style.width = "70%";
+    bar1[4].style.width = "40%";
+      } else {
+    bar1[0].style.width = "30%"
+    bar1[1].style.width = "30%";
+    bar1[2].style.width = "30%";
+    bar1[3].style.width = "30%";
+    bar1[4].style.width = "30%";
+    }
+});
