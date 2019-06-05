27a4d6a4a9adc209fa25b033da9ef90061df0e15
Reece <rfarnworth92@gmail.com>
2019-06-05 18:42:19 +0100

    more scrolling

--- resources/port.js
+++ resources/port.js
@@ -2,7 +2,10 @@ const mouse1 = document.getElementById('intro1')
 const mouse2 = document.getElementById('intro2')
 const mouse3 = document.getElementById('intro3')
 
+const bars = document.querySelector("#bar");
+const bar1 = document.querySelectorAll('.bartxt');
 
+/*----------------------------------------------------------------------intro X change---*/
 function mousemove(){
   var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
   mouse1.style.left = scrolltop * .5 + 'px' //add these px's * 50% to the left style
@@ -10,34 +13,52 @@ function mousemove(){
   mouse3.style.left = scrolltop * .5 + 'px'
 }
 
-document.addEventListener('scroll', function(){ // on page scroll
-  requestAnimationFrame(mousemove) // call mousemove  the animatione frame helps to smooth it out
-})
 
-
-/*------------------------------------TEST*/
-const bars = document.querySelector("#bar");
-const bar1 = document.querySelectorAll('.bartxt');
-const isInViewport = function (elem) {
-	const distance = elem.getBoundingClientRect();
+/*----------------------------------------------------------------------Bar animation---*/
+//is the element completely in view
+const isInViewport = function (e) {
+  //this gets its top, right, bottom and left coordinates
+	const distance = e.getBoundingClientRect();
 	return (
+    //We only need the top and bottom as the page doesnt scroll on the X axis
 		distance.top >= 0 &&
 		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight)
 	);
 };
 
-window.addEventListener('scroll', function (event) {
+/*----------------------------------------------------------------------TESTING---*/
+/*----------------------------------------------------------------------Smooth scroll---*/
+document.querySelector("a").onclick = function(){
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    document.querySelector("html, body").animate({
+      scrollTop: hash.offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    }
+  )
+  }
+}
+
+document.addEventListener('scroll', function(){ // on page scroll
+  requestAnimationFrame(mousemove) // call mousemove  the animatione frame helps to smooth it out
+
+  //if element has 0 top and bottom animate
   if (isInViewport(bars)) {
-		bar1[0].style.width = "80%"
+    bar1[0].style.width = "80%"
     bar1[1].style.width = "80%";
     bar1[2].style.width = "60%";
     bar1[3].style.width = "70%";
     bar1[4].style.width = "40%";
-      } else {
+  }//reset changes when out
+      else {
     bar1[0].style.width = "30%"
     bar1[1].style.width = "30%";
     bar1[2].style.width = "30%";
     bar1[3].style.width = "30%";
     bar1[4].style.width = "30%";
     }
-});
+})
